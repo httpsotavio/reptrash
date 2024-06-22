cf3ada576c23c168fdad6565f97ac8acd4b20f5b
httpsotavio <otavioaugustocardoso@gmail.com>
2024-06-22 03:33:10 -0300

    first commit

---
+++ gitmetadata.h
@@ -0,0 +1,20 @@
+// This file was created automatically by CMake.
+#pragma once
+
+// Whether or not we retrieved the state of the repo.
+#define GIT_RETRIEVED_STATE false
+
+// git describe --tags --match 'v*'
+#define GIT_DESCRIBE ""
+
+// The SHA1 for the HEAD of the repo.
+#define GIT_HEAD_SHA1 "7e319eb1c78a6c4c91fc0c07a8d9e036dfa126bd"
+
+// git rev-parse --short HEAD
+#define GIT_SHORT_SHA1 "7e319eb"
+
+// Whether or not there were uncommited changes present.
+#define GIT_IS_DIRTY true
+
+// When HEAD was committed.
+#define GIT_COMMIT_DATE_ISO8601 "2023-09-21T00:07:00-03:00"
